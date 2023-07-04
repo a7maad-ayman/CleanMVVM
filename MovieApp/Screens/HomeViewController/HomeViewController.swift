81b2f1817fd2bbd7c418ae09df1f4c5fb00e507c
Ahmad Ayman Mansour <87352168+a7maad-ayman@users.noreply.github.com>
2023-07-04 16:33:53 +0300

    feat:create a homeViewModel and test the api response (#4)
    Co-authored-by: a7maad-ayman <>

--- MovieApp/Screens/HomeViewController/HomeViewController.swift
+++ MovieApp/Screens/HomeViewController/HomeViewController.swift
@@ -6,22 +6,13 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var homeCollectionView: UICollectionView!
     
     //MARK: - Properties
-    var movies:Movies?
-    let endPoint = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.%20desc&api_key=1e95a9686ab126555e6f5df2ed967f7a"
+    private let viewModel = HomeViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGray4
-        NetworkService.shared.getMovies(from: endPoint, completion: { result in
-            switch result {
-            case .success(let movieArray):
-                self.movies = movieArray
-            case .failure(let error):
-                // Handle the error, if needed
-                print("Error: \(error.rawValue)")
-            }
-        })
-       
+        viewModel.showMovies()
+        print("\(viewModel.movies?.results?.count)")
     }
 }
 
