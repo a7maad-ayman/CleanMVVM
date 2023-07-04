884d5196e31190a3fc81a090f8ac25861fde6dd8
Ahmad Ayman Mansour <87352168+a7maad-ayman@users.noreply.github.com>
2023-07-04 12:56:46 +0300

    Feat/home screen (#1)
    * feat: delete storyboard
    
    * feat: delete storyboard
    
    * feat:module the app
    
    * feat:create a tabbar , and home screen
    
    * create a homeCollectionView , Extension for UIClollectionView for cell helper
    
    ---------
    
    Co-authored-by: ahmad <ahmad@P9W6-8-2.local>

---
+++ MovieApp/Screens/HomeViewController/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  MovieApp
+//
+//  Created by ahmad on 04/07/2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var homeCollectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
