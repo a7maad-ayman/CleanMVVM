061ec111c01ef8223a755617f1d0d6f4956dd59e
Ahmad Ayman Mansour <87352168+a7maad-ayman@users.noreply.github.com>
2023-07-04 16:08:04 +0300

    feat:create a tab bar with 2 nav controllers (#3)
    Co-authored-by: a7maad-ayman <>

---
+++ MovieApp/Cells/HomeCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  HomeCollectionViewCell.swift
+//  MovieApp
+//
+//  Created by ahmad on 04/07/2023.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
