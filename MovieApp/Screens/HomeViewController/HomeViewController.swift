d9e140c42286c1eed7eefe00a3da3f9bca307579
Ahmad Ayman Mansour <87352168+a7maad-ayman@users.noreply.github.com>
2023-07-05 15:50:33 +0300

    refactor : refactor binding (#7)
    * feat: fileration, details viewModel
    
    * refactor binding
    
    ---------
    
    Co-authored-by: a7maad-ayman <>

--- MovieApp/Screens/HomeViewController/HomeViewController.swift
+++ MovieApp/Screens/HomeViewController/HomeViewController.swift
@@ -1,98 +1,99 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+  
+  //MARK: - IBOutlets
+  @IBOutlet weak var homeCollectionView: UICollectionView!
+  
+  
+  //MARK: - Properties
+  private let viewModel = HomeViewModel()
+  
+  
+  //MARK: Life cycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureCollectionView()
+    bindViewModelState()
+    configureNavBar()
+    view.backgroundColor = .systemGray4
     
-    //MARK: - IBOutlets
-    @IBOutlet weak var homeCollectionView: UICollectionView!
+  }
+  override func viewWillAppear(_ animated: Bool) {
+    viewModel.getMovies()
+  }
+  
+  
+  func configureCollectionView() {
+    homeCollectionView.delegate = self
+    homeCollectionView.dataSource = self
+    homeCollectionView.register(HomeCollectionViewCell.self)
     
-    
-    //MARK: - Properties
-    private let viewModel = HomeViewModel()
-    
-    
-    //MARK: Life cycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        configureCollectionView()
-        configureOnReloadData()
-        configureNavBar()
-        view.backgroundColor = .systemGray4
+  }
+  
+  func bindViewModelState() {
+    viewModel.reload = { [weak self] in
+      DispatchQueue.main.async {
+        self?.homeCollectionView.reloadData()
+      }
+    }
+  }
+  
+  func configureNavBar() {
+    let menu = UIMenu(title: "Filter", children: [
+      UIAction(title: "Most Viewed", handler: { _ in
         
-    }
-    override func viewWillAppear(_ animated: Bool) {
-        homeCollectionView.reloadData()
-    }
-   
+        self.viewModel.getMovies(by: Constants.mostViewed)
+        DispatchQueue.main.async {
+          self.homeCollectionView.reloadData()
+        }            }),
+      UIAction(title: "Popularity", handler: { _ in
+        self.viewModel.getMovies(by: Constants.popularity)
+        DispatchQueue.main.async {
+          self.homeCollectionView.reloadData()
+        }            }),
+    ])
+    navigationItem.rightBarButtonItem = UIBarButtonItem(title: nil, image: UIImage(systemName: "list.bullet"), primaryAction: nil, menu: menu)
     
-     func configureCollectionView() {
-        homeCollectionView.delegate = self
-        homeCollectionView.dataSource = self
-         homeCollectionView.register(HomeCollectionViewCell.self)
-
-    }
-    
-     func configureOnReloadData() {
-        viewModel.configureOnReloadNeeded {
-            DispatchQueue.main.async {
-                self.homeCollectionView.reloadData()
-            }
-        }
-        viewModel.getMovies()
-    }
-    
-    func configureNavBar() {
-        let menu = UIMenu(title: "Filter", children: [
-            UIAction(title: "Most Viewed", handler: { _ in
-
-                self.viewModel.getMovies(by: Constants.mostViewed)
-                DispatchQueue.main.async {
-                    self.homeCollectionView.reloadData()
-                }            }),
-            UIAction(title: "Popularity", handler: { _ in
-                self.viewModel.getMovies(by: Constants.popularity)
-                DispatchQueue.main.async {
-                    self.homeCollectionView.reloadData()
-                }            }),
-        ])
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: nil, image: UIImage(systemName: "list.bullet"), primaryAction: nil, menu: menu)
-
-    }
-
+  }
+  
 }
 
 //MARK: - UICollectionViewDataSource
 extension HomeViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        viewModel.numberOfRows()
-    }
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    viewModel.numberOfRows
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        let cell:HomeCollectionViewCell = collectionView.dequeueReusableCell(for: indexPath)
-        cell.set(movie: viewModel.allMovies[indexPath.row])
-        
-        return cell
-    }
+    let cell:HomeCollectionViewCell = collectionView.dequeueReusableCell(for: indexPath)
+    cell.set(movie: viewModel.getItem(at: indexPath.row))
     
-    
+    return cell
+  }
+  
+  
 }
 
 //MARK: - UICollectionViewDelegate
 extension HomeViewController: UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let viewController =  DetailsViewController(viewModel: DetailsViewModel(movie: viewModel.allMovies[indexPath.row]))
-        self.navigationController?.pushViewController(viewController, animated: true)
-    }
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    guard let item = viewModel.getItem(at: indexPath.row) else { return }
+    let viewModel = DetailsViewModel(movie: item)
+    let viewController =  DetailsViewController(viewModel: viewModel)
+    self.navigationController?.pushViewController(viewController, animated: true)
+  }
 }
 
 //MARK: - UICollectionViewDelegate
 extension HomeViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-            return CGSize(width: self.view.frame.width/3 - 30, height: self.view.frame.height * 0.18)
-        }
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-      let margin: CGFloat = 10
-      return UIEdgeInsets(top: margin, left: margin, bottom: margin, right: margin)
-    }
-
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    return CGSize(width: self.view.frame.width/3 - 30, height: self.view.frame.height * 0.18)
+  }
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+    let margin: CGFloat = 10
+    return UIEdgeInsets(top: margin, left: margin, bottom: margin, right: margin)
+  }
+  
 }
