4b898e6194b89fde139b5b23c0c1108a01208063
a7maad-ayman <ahmad@P9W6-8-2.local>
2023-07-09 15:39:45 +0300

    refactor: apply clean arch

--- MovieApp/Application/SceneDelegate.swift
+++ MovieApp/Application/SceneDelegate.swift
@@ -11,34 +11,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = createTabBar()
+        window?.rootViewController = createHomeNC()
         window?.makeKeyAndVisible()
     }
     
     func createHomeNC() -> UINavigationController {
             let homeVC = HomeViewController()
-        homeVC.title = "Home"
-        homeVC.tabBarItem = UITabBarItem(tabBarSystemItem:.recents, tag: 0)
-            
             return UINavigationController(rootViewController: homeVC)
         }
-        
-        func createFavoritesNC() -> UINavigationController {
-            let favoritesVC = FavoritesViewController()
-            favoritesVC.title = "Favorites"
-            favoritesVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
-            
-            return UINavigationController(rootViewController: favoritesVC)
-        }
-    
-    
-    func createTabBar() -> UITabBarController {
-            let tabbar = UITabBarController()
-            UITabBar.appearance().tintColor = .systemGreen
-            UITabBar.appearance().backgroundColor = .systemGray4
-            tabbar.viewControllers = [createHomeNC(),createFavoritesNC()]
-            return tabbar
-        }
-
 }
 
